74209230cf2c560070565e48188de43372a27ccb
nicolaslazo <45973144+nicolaslazo@users.noreply.github.com>
2025-07-29 19:44:52 -0300

    Simplification tolerance tweak

--- migrations/090-simplify-geoms.sql
+++ migrations/090-simplify-geoms.sql
@@ -5,7 +5,7 @@ BEGIN;
 WITH simplified AS (
   SELECT 
     ogr_fid, 
-    ST_CoverageSimplify(geom, 0.000003) OVER (PARTITION BY seccion, manzana) AS simplified_geom
+    ST_CoverageSimplify(geom, 0.000002) OVER (PARTITION BY seccion, manzana) AS simplified_geom
   FROM city_data
 )
 UPDATE city_data cd
